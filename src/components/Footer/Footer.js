7160c5382f8f083652f1921f7f8eb42a21da4605
Afif Akromi <massachusetts48@gmail.com>
2021-12-01 22:31:50 +0700

    init project

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
@@ -10,12 +20,12 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:314-343-3432">081395828364</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
           <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
+            13519110@std.stei.itb.ac.id
           </LinkItem>
         </LinkColumn>
       </LinkList>
@@ -24,13 +34,13 @@ const Footer = () => {
           <Slogan>Innovating one project at a time</Slogan>
         </CompanyContainer>
         <SocialContainer>
-          <SocialIcons href="https://google.com">
+          <SocialIcons href="https://github.com/akromiafif">
             <AiFillGithub size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://google.com">
+          <SocialIcons href="https://www.linkedin.com/in/afif-akromi-837a05206/">
             <AiFillLinkedin size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://google.com">
+          <SocialIcons href="https://www.instagram.com/akromiafif/">
             <AiFillInstagram size="3rem" />
           </SocialIcons>
         </SocialContainer>
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import { AiOutlineMobile } from "react-icons/ai";
+import { FiPackage } from "react-icons/fi";
+import { MdLibraryBooks } from "react-icons/md";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
+const Technologies = () => (
   <Section id="tech">
     <SectionDivider divider />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      I've worked with a range a technologies in the web development world.
-      From Back-end To Design
+      I've worked with a range a technologies in the web development world. From
+      Back-end To Design
     </SectionText>
     <List>
       <ListItem>
@@ -20,7 +34,11 @@ const Technologies = () =>  (
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
             Experiece with <br />
-            React.js
+            React JS <br />
+            Angular JS <br />
+            Electron JS <br />
+            NEXT JS <br />
+            Nest JS <br />
           </ListParagraph>
         </ListContainer>
       </ListItem>
@@ -32,7 +50,12 @@ const Technologies = () =>  (
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
             Experience with <br />
-            Node and Databases
+            Node JS <br />
+            Laravel <br />
+            Django REST Framework <br />
+            Flask <br />
+            Firebase <br />
+            MongoDB <br />
           </ListParagraph>
         </ListContainer>
       </ListItem>
@@ -44,7 +67,53 @@ const Technologies = () =>  (
           <ListTitle>UI/UX</ListTitle>
           <ListParagraph>
             Experience with <br />
-            tools like Figma
+            Figma <br />
+            Adobe XD <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <AiOutlineMobile size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Mobile-Dev</ListTitle>
+          <ListParagraph>
+            Experiece with <br />
+            Flutter <br />
+            React Native <br />
+            Kotlin & Java <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <FiPackage size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>3Party-Library</ListTitle>
+          <ListParagraph>
+            Experiece with <br />
+            Passport JS <br />
+            Recoil JS <br />
+            Three JS <br />
+            TypeORM <br />
+            Sequilze <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <MdLibraryBooks size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Others</ListTitle>
+          <ListParagraph>
+            Experiece with <br />
+            Keras Python <br />
+            OpenCV <br />
+            ROS Framework <br />
+            TensorFlow <br />
           </ListParagraph>
         </ListContainer>
       </ListItem>
